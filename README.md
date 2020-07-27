7d9fd09798ffd5f0bd042f2628787532698cf6a9
AizOfMulia <aizofmulia@gmail.com>
2020-07-27 22:03:27 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# IEB 20603 DATABASE MANAGEMENT SYSTEM
+
+## Group assignment
+
+### Requirement(s)
+1. Docker
+2. Docker Compose
+
+### How to use
+
+1. Clone the repo `git clone https://`
+2. Cd into the directory.
+3. Run `docker-compose up -d`
+4. Shut down the server `docker-compose down`
+
+### Laravel features
+The project uses laravel as a MVC framework, the basic folders used are
+1. `app` (MODEL)
+This folders contain the model and data representation of a table.
+
+2. `resource/views` (VIEW)
+This folder contains all the frontend design templates and html files.
+[View Doc](https://laravel.com/docs/7.x/blade)
+
+2. `app/Http/Controllers` (CONTROLLER)
+This folder contains all the backend logic and data manipulations. i.e: routing
+[Controller Doc](https://laravel.com/docs/7.x/controllers)
+
+3. `database/migrations` - Migration
+[Migrations Doc](https://laravel.com/docs/7.x/migrations)
+
+4. `routes/web.php`
+All the routes and configured URI are kept within the file
+[Routing Doc](https://laravel.com/docs/7.x/routing)
---
+++ lara-app/database/migrations/2020_07_28_000000_create_users_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) 
+        {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->unsignedBiginteger('roles_id')->default(1);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        /**
+         *
+         * Creates the relationship between tables
+         *
+         * @return void
+         */
+        Schema::table('users', function (Blueprint $table)
+        {
+            $table->foreign('roles_id')
+                  ->references('id')
+                  ->on('users_roles')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
