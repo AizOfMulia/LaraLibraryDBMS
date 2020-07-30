0d2e8c99993ad797e13f4a8370b43670e7ab2e05
phineasandferb97radzi <phineasandferb97radzi@gmail.com>
2020-07-30 13:58:22 +0800

    Add PageController, Edit Route and add Page for list and view for book

--- lara-app/routes/web.php
+++ lara-app/routes/web.php
@@ -13,9 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->middleware('auth');
+Route::get('/', 'PageController@dashboard')->middleware('auth');
+
+Route::get('/InsertBook', 'PageController@insertBook');
+
+Route::get('/ListBook', 'PageController@listBook');
 
 //Route::get('/login', 'LoginController@showLogin');
 
