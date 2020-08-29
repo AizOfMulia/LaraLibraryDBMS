f17e6dfd2f52f7f030b8b8f0dd41436c9c25e262
AizOfMulia <aizofmulia@gmail.com>
2020-08-30 04:18:38 +0800

    Added staff controller and minor routing

--- lara-app/app/Http/Controllers/Auth/LoginController.php
+++ lara-app/app/Http/Controllers/Auth/LoginController.php
@@ -36,5 +36,64 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+        $this->middleware('guest:staff')->except('logout');
+    }
+
+    /**
+     *  Show the default login page
+     *
+     *  @return \Illuminate\View\View
+     */
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    /**
+     *  Show the staff login page
+     *
+     *  @return \Illuminate\View\View
+     */
+    public function showStaffLogin()
+    {
+        return view('auth.login', ['url' => 'staff']);
+    }
+
+    /**
+     *  Login in staff and validate their information before attempting
+     *
+     *  @return \Illuminate\Redirect
+     */
+    public function loginStaff(Request $request)
+    {
+        $this->validateLogin($request);
+
+        if(method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request))
+        {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if(attemptLoginStaff())
+            return redirect()->intended('/');
+
+        return back()->withInput($request->only($this->username(), 'remember'));
+    }
+
+    /**
+     *  Attempts to login in staff accounts
+     *
+     *  @return bool
+     */
+    public function attemptLoginStaff($request)
+    {
+        return Auth::guard('staff')
+            ->attempt([
+                'email' => $request->email,
+                'password' => $request->password],
+                $request->get('remember')
+            );
     }
 }
--- lara-app/routes/web.php
+++ lara-app/routes/web.php
@@ -17,9 +17,16 @@ Route::get('/', function () {
     return view('welcome');
 })->middleware('auth');
 
-//Route::get('/login', 'LoginController@showLogin');
 
 Auth::routes();
+
+Route::group(['prefix' => 'login', 'as' => 'login.'], function ()
+{
+    Route::get('/', 'Auth\LoginController@showLoginForm')->name('show.user');
+    Route::post('/', 'Auth\LoginController@login')->name('user');
+    Route::get('/staff', 'Auth\LoginController@showStaffLogin')->name('show.staff');
+    Route::post('/staff', 'Auth\LoginController@loginStaff')->name('staff');
+});
 
 Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
 Route::post('/user', 'UserController@searchUser')->name('searchUser');
