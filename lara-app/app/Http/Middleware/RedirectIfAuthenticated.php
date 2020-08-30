35179fcc21bfa6003e0b8b181868a125decbcb75
AizOfMulia <aizofmulia@gmail.com>
2020-08-31 02:03:58 +0800

    Added correct middleware redirects

--- lara-app/app/Http/Middleware/RedirectIfAuthenticated.php
+++ lara-app/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,9 +18,11 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
+        if ($guard == "staff" && Auth::guard($guard)->check())
+            return redirect('/');
+
+        if (Auth::guard($guard)->check())
             return redirect(RouteServiceProvider::DASHBOARD);
-        }
 
         return $next($request);
     }
